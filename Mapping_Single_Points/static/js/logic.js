0c689e19f41156e1aee5afbc59cedff2ff01efb4
unknown <ajawa_took@yahoo.com>
2022-09-11 12:43:51 -0700

    nother branch

---
+++ Mapping_Single_Points/static/js/logic.js
@@ -0,0 +1,43 @@
+// Add console.log to check to see if our code is working.
+console.log("I am logic.js , what do you want?");
+
+const dom = [55.7428868441541, 37.615418005983436];
+const flyover = [40.7, -94.5];
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView(dom, 15);
+
+
+
+// another way to load request tiles
+// // We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox/streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: API_KEY
+// });
+ 
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+
+
+
+
+
+
+
+
+
+
+
