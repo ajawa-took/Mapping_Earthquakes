26ee1527175be5ae908997a5cab4929603522726
unknown <ajawa_took@yahoo.com>
2022-09-18 15:19:32 -0700

    single marker

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -3,11 +3,28 @@ console.log("I am logic.js , what do you want?");
 
 const dom = [55.7428868441541, 37.615418005983436];
 const flyover = [40.7, -94.5];
+const centerLA = [34.0522, -118.2437];
+const offCenter = [34.05, -118.24];
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView(dom, 15);
+let map = L.map('mapid').setView(centerLA, 14);
 
+// //  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker(centerLA).addTo(map);
 
+// radius in meters
+L.circle(centerLA, {
+    color: 'black',
+    fillColor: 'yellow',
+    radius: 300
+ }).addTo(map);
+
+ // radius in pixels
+ L.circleMarker(offCenter, {
+    color: 'black',
+    fillColor: 'green',
+    radius: 30
+ }).addTo(map);
 
 // another way to load request tiles
 // // We create the tile layer that will be the background of our map.
@@ -21,7 +38,8 @@ let map = L.map('mapid').setView(dom, 15);
 // });
  
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// flavor of map -- streets, dark, terrain -- is in the first url after "/mapbox/"
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
