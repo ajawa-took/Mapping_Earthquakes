10bafbb30ea0390155e8db6d3d121ff8ef7b8d79
ajawa-took <ajawa_took@yahoo.com>
2022-12-02 21:41:41 -0800

    modules done

--- Earthquake_Challenge/static/js/logic.js
+++ Earthquake_Challenge/static/js/logic.js
@@ -51,6 +51,27 @@ let overlays = {
 // which layers are visible.
 L.control.layers(baseMaps, overlays).addTo(map);
 
+// Now we build a legend
+// Create a legend control object.
+let legend = L.control({  position: "bottomright" });
+
+// Then add all the details for the legend.
+legend.onAdd = function() {
+  let div = L.DomUtil.create("div", "info legend");
+  const magnitudes = [0, 1, 2, 3, 4, 5];
+  const colors = [  "#98ee00",  "#d4ee00",  "#eecc00",  "#ee9c00",  "#ea822c",  "#ea2c2c"  ];
+  // Looping through our intervals to generate a label with a colored square for each interval.
+  for (var i = 0; i < magnitudes.length; i++) {
+    console.log(colors[i]);
+    div.innerHTML +=
+      "<i style='background: " + colors[i] + "'></i> " +
+      magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+  }
+  return div;
+};
+
+legend.addTo(map);
+
 
 let quakes_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
@@ -102,8 +123,6 @@ d3.json(quakes_url).then(function(data) {
     }
     return "#98ee00";
   }  
-
-
 
   // // Creating a GeoJSON layer with the retrieved data, simplest
   // L.geoJSON(data).addTo(map);
