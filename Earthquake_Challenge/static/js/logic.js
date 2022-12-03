20e7441fec59f28ea39992551c13af3b0abb0110
ajawa-took <ajawa_took@yahoo.com>
2022-12-02 20:42:14 -0800

    done 13.6.3

--- Earthquake_Challenge/static/js/logic.js
+++ Earthquake_Challenge/static/js/logic.js
@@ -67,7 +67,7 @@ d3.json(quakes_url).then(function(data) {
     return {
       opacity: 1,
       fillOpacity: 1,
-      fillColor: "#ffae42",
+      fillColor: getColor(feature.properties.mag), // "#ffae42"
       color: "#000000",
       radius: getRadius(feature.properties.mag),
       stroke: true,
@@ -84,6 +84,28 @@ d3.json(quakes_url).then(function(data) {
     return magnitude * 4;
   }
 
+  // This function determines the color of the circle based on the magnitude of the earthquake.
+  function getColor(magnitude) {
+    if (magnitude > 5) {
+      return "#ea2c2c";
+    }
+    if (magnitude > 4) {
+      return "#ea822c";
+    }
+    if (magnitude > 3) {
+      return "#ee9c00";
+    }
+    if (magnitude > 2) {
+      return "#eecc00";
+    }
+    if (magnitude > 1) {
+      return "#d4ee00";
+    }
+    return "#98ee00";
+  }  
+
+
+
   // // Creating a GeoJSON layer with the retrieved data, simplest
   // L.geoJSON(data).addTo(map);
   // Less simple: turn each feature into a circleMarker on the map.
@@ -93,7 +115,14 @@ d3.json(quakes_url).then(function(data) {
               return L.circleMarker(latlng);
               },
             // We set the style for each circleMarker using our styleInfo function.
-            style: styleInfo
+            style: styleInfo,
+            // We create a popup for each circleMarker to display the magnitude and
+            //  location of the earthquake after the marker has been created and styled.
+            onEachFeature: function(feature, layer) {
+                layer.bindPopup("Magnitude: " + feature.properties.mag + 
+                "<br>Location: " + feature.properties.place+
+                "<br>Date: " + new Date(feature.properties.time).toLocaleDateString('en-US'));
+                }
             }
             ).addTo(map);
 });
